5fbb86a57798e97e3108fd0eea37ed3bae4eeae7
Antonio Parraga <aparraga@lionframework.org>
2012-01-14 13:51:36 +0100

    Just update the year in a doc-block

--- lion/lion.php
+++ lion/lion.php
@@ -2,7 +2,7 @@
 /**
  * This file is part of lion framework.
  * 
- * Copyright (c) 2011 Antonio Parraga Navarro
+ * Copyright (c) 2012 Antonio Parraga Navarro
  * 
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
