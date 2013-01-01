078606bcfdf9a257b80396705b5a39f8db81037d
Antonio Parraga Navarro <antonio@parraga.es>
2013-01-01 23:44:26 +0100

    just added the a tag as component when runat = server

--- lion/libs/mvc/componentmodel/render/RunAtServerHtmlElementHelper.class.php
+++ lion/libs/mvc/componentmodel/render/RunAtServerHtmlElementHelper.class.php
@@ -5,6 +5,9 @@ class __RunAtServerHtmlElementHelper {
     static public function resolveComponentTag($html_tag_name, $attributes) {
         $return_value = null;
         switch($html_tag_name) {
+            case 'a':
+                $return_value = 'commandlink';
+                break;
             case 'form':
                 $return_value = 'form';
                 break;
